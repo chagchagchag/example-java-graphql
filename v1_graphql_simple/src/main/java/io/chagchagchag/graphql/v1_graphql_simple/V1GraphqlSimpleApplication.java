8049e3a59d81b1e5cbeea3bfe818266eb98753d1
chagchagchag <chagchagchag.dev@gmail.com>
2024-06-09 11:09:00 +0900

    v1_graphql_simple

---
+++ v1_graphql_simple/src/main/java/io/chagchagchag/graphql/v1_graphql_simple/V1GraphqlSimpleApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.graphql.v1_graphql_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1GraphqlSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1GraphqlSimpleApplication.class, args);
+	}
+
+}
